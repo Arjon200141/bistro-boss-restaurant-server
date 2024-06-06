a1f03184cddd8380651ecd728182bdc1e5506434
Arjon200141 <aorjon123@gmail.com>
2024-06-06 13:22:04 +0600

    TansTack Query

--- index.js
+++ index.js
@@ -27,13 +27,25 @@ async function run() {
 
         const menuCollection = client.db("bistroDB").collection("menu");
         const reviewsCollection = client.db("bistroDB").collection("reviews");
+        const cartCollection = client.db("bistroDB").collection("carts");
         
         app.get("/menu" , async(req,res) => {
             const result =await menuCollection.find().toArray();
             res.send(result);
         })
         app.get("/reviews" , async(req,res) => {
-            const result =await  reviewsCollection.find().toArray();
+            const result =await reviewsCollection.find().toArray();
+            res.send(result);
+        }) 
+
+        app.get("/carts" , async(req,res) => {
+            const result =await cartCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.post('/carts' , async(req,res) =>{
+            const cartItem = req.body;
+            const result = await cartCollection.insertOne(cartItem);
             res.send(result);
         })
 
